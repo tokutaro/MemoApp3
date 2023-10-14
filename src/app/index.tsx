81e79d6a4101645b17a3cb11284cc7a0c53a52c5
tokutaro <22991683+tokutaro@users.noreply.github.com>
2023-10-14 09:39:16 +0900

    メモリストのレイアウトを整えた

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,41 +4,39 @@ const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
 
-      <View>
-        <View>
-          <Text>Memo APPP</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo APPP</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
       <View>
-        <View>
-          <Text>かいものりすと</Text>
-          <Text>2023年10月4日 11:23</Text>
-        </View>
-        <View>
-          <Text>×ボタン</Text>
+
+      <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>かいものりすと</Text>
+            <Text style={styles.memoListItemDate}>2023年10月4日 11:23</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
         </View>
 
-        <View>
-          <Text>かいものりすと</Text>
-          <Text>2023年10月4日 11:23</Text>
-        </View>
-        <View>
-          <Text>×ボタン</Text>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>かいものりすと</Text>
+            <Text style={styles.memoListItemDate}>2023年10月4日 11:23</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
         </View>
 
-        <View>
-          <Text>かいものりすと</Text>
-          <Text>2023年10月4日 11:23</Text>
-        </View>
-        <View>
-          <Text>×ボタン</Text>
-        </View>
       </View>
 
-      <View>
-        <Text>＋</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>＋</Text>
       </View>
 
     </View>
@@ -47,9 +45,69 @@ const Index = (): JSX.Element => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#FFFFFF'
+  },
+  memoListItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 40,
+    right: 40,
+
+    // iOSのための影のスタイル
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    // Androidのための影のスタイル
+    elevation: 10
+  },
+  circleButtonLabel: {
+    color: '#FFFFFF',
+    fontSize: 40
   }
 })
 
