f5c2f0039dfa9e12384c1e97482ae33bc4de2fe8
tokutaro <22991683+tokutaro@users.noreply.github.com>
2023-10-18 12:00:15 +0900

    ログイン画面つくった

---
+++ src/app/auth/login.tsx
@@ -0,0 +1,64 @@
+import { Text, View, StyleSheet, TextInput } from 'react-native'
+import Header from '../../components/Header'
+import Button from '../../components/Button'
+
+const logIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Loginnnnnn</Text>
+        <TextInput style={styles.input} value='email' />
+        <TextInput style={styles.input} value='password' />
+
+        <Button label='login'/>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>not registerd?</Text>
+          <Text style={styles.footerLink}>sign up here</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8'
+  },
+  inner: {
+    paddingVertical: 24,
+    paddingHorizontal: 27
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24
+  },
+  input: {
+    backgroundColor: 'orange',
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+    height: 48,
+    padding: 8,
+    fontSize: 16,
+    marginBottom: 16
+  },
+  footer: {
+    flexDirection: 'row'
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3'
+  }
+})
+
+export default logIn
